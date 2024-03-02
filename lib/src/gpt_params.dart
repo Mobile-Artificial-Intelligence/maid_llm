4640229fc40e8471ee7901f5867be396834193fe
danemadsen <dane_madsen@hotmail.com>
2024-03-02 20:44:14 +1000

    work on maid_llm

--- lib/src/gpt_params.dart
+++ lib/src/gpt_params.dart
@@ -6,254 +6,254 @@ import 'bindings.dart';
 import 'package:ffi/ffi.dart';
 
 class GptParams {
-  // Seed for random number generation. Defaults to a random integer.
-  // A value of -1 indicates a random seed should be used.
+  /// Seed for random number generation. Defaults to a random integer.
+  /// A value of -1 indicates a random seed should be used.
   int seed = Random().nextInt(1000000);
 
-  // Number of threads to use for generation. Defaults to 8.
+  /// Number of threads to use for generation. Defaults to 8.
   int nThreads = 8;
 
-  // Possibly unused. Defaults to -1.
+  /// Possibly unused. Defaults to -1.
   int nThreadsDraft = -1;
 
-  // Number of threads to use for batch processing. Defaults to 8.
+  /// Number of threads to use for batch processing. Defaults to 8.
   int nThreadsBatch = 8;
 
-  // Possibly unused. Defaults to -1.
+  /// Possibly unused. Defaults to -1.
   int nThreadsBatchDraft = -1;
 
-  // Number of predictions to generate. Defaults to -1.
+  /// Number of predictions to generate. Defaults to -1.
   int nPredict = -1;
 
-  // Number of tokens in the context. Defaults to 512.
+  /// Number of tokens in the context. Defaults to 512.
   int nCtx = 512;
 
-  // Number of tokens in the batch. Defaults to 512.
+  /// Number of tokens in the batch. Defaults to 512.
   int nBatch = 512;
 
-  // Number of tokens to keep from initial prompt. Defaults to 0.
+  /// Number of tokens to keep from initial prompt. Defaults to 0.
   int nKeep = 0;
 
-  // Number of tokens to keep from draft. Defaults to 8.
+  /// Number of tokens to keep from draft. Defaults to 8.
   int nDraft = 8;
 
-  // Maximum number of chunks to process. Defaults to -1.
+  /// Maximum number of chunks to process. Defaults to -1.
   int nChunks = -1;
 
-  // Number of parallel sequences to decode. Defaults to 1.
+  /// Number of parallel sequences to decode. Defaults to 1.
   int nParallel = 1;
 
-  // Number of sequences to decode. Defaults to 1.
+  /// Number of sequences to decode. Defaults to 1.
   int nSequences = 1;
 
-  // Speculative decoding accept probability. Defaults to 0.5.
+  /// Speculative decoding accept probability. Defaults to 0.5.
   double pAccept = 0.5;
 
-  // Speculative decoding split probability. Defaults to 0.1.
+  /// Speculative decoding split probability. Defaults to 0.1.
   double pSplit = 0.1;
 
-  // Number of layers to store in VRAM. Defaults to -1.
+  /// Number of layers to store in VRAM. Defaults to -1.
   int nGpuLayers = -1;
 
-  // Number of layers to store in VRAM for the draft model. Defaults to -1.
+  /// Number of layers to store in VRAM for the draft model. Defaults to -1.
   int nGpuLayersDraft = -1;
 
-  // How to split the model across GPUs. Defaults to SplitMode.layer.
+  /// How to split the model across GPUs. Defaults to SplitMode.layer.
   SplitMode splitMode = SplitMode.layer;
 
-  // The GPU that is used for scratch and small tensors. Defaults to 0.
+  /// The GPU that is used for scratch and small tensors. Defaults to 0.
   int mainGpu = 0;
 
-  // How split tensors should be distributed across GPUs. Defaults to an empty list.
+  /// How split tensors should be distributed across GPUs. Defaults to an empty list.
   List<double> tensorSplit = [];
 
-  // If non-zero then use beam search of given width. Defaults to 0.
+  /// If non-zero then use beam search of given width. Defaults to 0.
   int nBeams = 0;
 
-  // Group-attention factor. Defaults to 1.
+  /// Group-attention factor. Defaults to 1.
   int grpAttnN = 1;
 
-  // Group-attention width. Defaults to 512.
+  /// Group-attention width. Defaults to 512.
   int grpAttnW = 512;
 
-  // Print token count every n tokens. Defaults to -1.
+  /// Print token count every n tokens. Defaults to -1.
   int nPrint = -1;
 
-  // RoPE base frequency. Defaults to 0.0.
+  /// RoPE base frequency. Defaults to 0.0.
   double ropeFreqBase = 0.0;
 
-  // RoPE frequency scaling factor. Defaults to 0.0.
+  /// RoPE frequency scaling factor. Defaults to 0.0.
   double ropeFreqScale = 0.0;
 
-  // YaRN extrapolation mix factor. Defaults to -1.0.
+  /// YaRN extrapolation mix factor. Defaults to -1.0.
   double yarnExtFactor = -1.0;
 
-  // YaRN magnitude scaling factor. Defaults to 1.0.
+  /// YaRN magnitude scaling factor. Defaults to 1.0.
   double yarnAttnFactor = 1.0;
 
-  // YaRN low correction dim. Defaults to 32.0.
+  /// YaRN low correction dim. Defaults to 32.0.
   double yarnBetaFast = 32.0;
 
-  // YaRN high correction dim. Defaults to 1.0.
+  /// YaRN high correction dim. Defaults to 1.0.
   double yarnBetaSlow = 1.0;
 
-  // YaRN original context length. Defaults to 0.
+  /// YaRN original context length. Defaults to 0.
   int yarnOrigCtx = 0;
 
-  // KV cache defragmentation threshold. Defaults to -1.0.
+  /// KV cache defragmentation threshold. Defaults to -1.0.
   double defragThold = -1.0;
 
-  // Rope scaling type. Defaults to -1.
+  /// Rope scaling type. Defaults to -1.
   int ropeScalingType = -1;
 
-  // NUMA strategy. Defaults to GgmlNumaStrategy.disabled.
+  /// NUMA strategy. Defaults to GgmlNumaStrategy.disabled.
   GgmlNumaStrategy numa = GgmlNumaStrategy.disabled;
 
-  // Model path.
+  /// Model path.
   String model = '';
 
-  // Draft model for speculative decoding.
+  /// Draft model for speculative decoding.
   String modelDraft = '';
 
-  // Model alias.
+  /// Model alias.
   String modelAlias = '';
 
-  // Prompt.
+  /// Prompt.
   String prompt = '';
 
-  // Store the external prompt file name.
+  /// Store the external prompt file name.
   String promptFile = '';
 
-  // Path to file for saving/loading prompt eval state.
+  /// Path to file for saving/loading prompt eval state.
   String pathPromptCache = '';
 
-  // String to prefix user inputs with.
+  /// String to prefix user inputs with.
   String inputPrefix = '';
 
-  // String to suffix user inputs with.
+  /// String to suffix user inputs with.
   String inputSuffix = '';
 
-  // String upon seeing which more user input is prompted.
+  /// String upon seeing which more user input is prompted.
   String antiprompt = '';
 
-  // Directory in which to save YAML log files.
+  /// Directory in which to save YAML log files.
   String logdir = '';
 
-  // File for saving *all* logits.
+  /// File for saving *all* logits.
   String logitsFile = '';
 
-  // Base model path for the lora adapter.
+  /// Base model path for the lora adapter.
   String loraBase = '';
 
-  // Stride for perplexity calculations. If left at 0, the pre-existing approach will be used.
+  /// Stride for perplexity calculations. If left at 0, the pre-existing approach will be used.
   int pplStride = 0;
 
-  // = 0 -> ppl output is as usual, = 1 -> ppl output is num_tokens, ppl, one per line
+  /// = 0 -> ppl output is as usual, = 1 -> ppl output is num_tokens, ppl, one per line
   int pplOutputType = 0;
 
-  // Compute HellaSwag score over random tasks from datafile supplied in prompt.
+  /// Compute HellaSwag score over random tasks from datafile supplied in prompt.
   bool hellaswag = false;
 
-  // Number of tasks to use when computing the HellaSwag score.
+  /// Number of tasks to use when computing the HellaSwag score.
   int hellaswagTasks = 400;
 
-  // Compute Winogrande score over random tasks from datafile supplied in prompt.
+  /// Compute Winogrande score over random tasks from datafile supplied in prompt.
   bool winogrande = false;
 
-  // Number of tasks to use when computing the Winogrande score. If 0, all tasks will be computed.
+  /// Number of tasks to use when computing the Winogrande score. If 0, all tasks will be computed.
   int winograndeTasks = 0;
 
-  // Compute TruthfulQA score over random tasks from datafile supplied in prompt.
+  /// Compute TruthfulQA score over random tasks from datafile supplied in prompt.
   bool multipleChoice = false;
 
-  // Number of tasks to use when computing the TruthfulQA score. If 0, all tasks will be computed.
+  /// Number of tasks to use when computing the TruthfulQA score. If 0, all tasks will be computed.
   int multipleChoiceTasks = 0;
 
-  // Compute KL-divergence.
+  /// Compute KL-divergence.
   bool klDivergence = false;
 
-  // If true, use mul_mat_q kernels instead of cuBLAS.
+  /// If true, use mul_mat_q kernels instead of cuBLAS.
   bool mulMatQ = true;
 
-  // Do not randomize prompt if none provided.
+  /// Do not randomize prompt if none provided.
   bool randomPrompt = false;
 
-  // Use color to distinguish generations and inputs.
+  /// Use color to distinguish generations and inputs.
   bool useColor = false;
 
-  // Interactive mode.
+  /// Interactive mode.
   bool interactive = false;
 
-  // Chatml mode (used for models trained on chatml syntax).
+  /// Chatml mode (used for models trained on chatml syntax).
   bool chatml = false;
 
-  // Save user input and generations to prompt cache.
+  /// Save user input and generations to prompt cache.
   bool promptCacheAll = false;
 
-  // Open the prompt cache read-only and do not update it.
+  /// Open the prompt cache read-only and do not update it.
   bool promptCacheRo = false;
 
-  // Get only sentence embedding.
+  /// Get only sentence embedding.
   bool embedding = false;
 
-  // Escape "\n", "\r", "\t", "\'", "\"", and "\\".
+  /// Escape "\n", "\r", "\t", "\'", "\"", and "\\".
   bool escape = false;
 
-  // Wait for user input immediately.
+  /// Wait for user input immediately.
   bool interactiveFirst = false;
 
-  // Reverse the usage of `\`.
+  /// Reverse the usage of `\`.
   bool multilineInput = false;
 
-  // Improves compatibility with subprocesses and limited consoles.
+  /// Improves compatibility with subprocesses and limited consoles.
   bool simpleIo = false;
 
-  // Insert new sequences for decoding on-the-fly.
+  /// Insert new sequences for decoding on-the-fly.
   bool contBatching = false;
 
-  // Prefix BOS to user inputs, preceding input_prefix.
+  /// Prefix BOS to user inputs, preceding input_prefix.
   bool inputPrefixBos = false;
 
-  // Ignore generated EOS tokens.
+  /// Ignore generated EOS tokens.
   bool ignoreEos = false;
 
-  // Instruction mode (used for Alpaca models).
+  /// Instruction mode (used for Alpaca models).
   bool instruct = false;
 
-  // Return logits for all tokens in the batch.
+  /// Return logits for all tokens in the batch.
   bool logitsAll = false;
 
-  // Use mmap for faster loads.
+  /// Use mmap for faster loads.
   bool useMmap = true;
 
-  // Use mlock to keep model in memory.
+  /// Use mlock to keep model in memory.
   bool useMlock = false;
 
-  // Print prompt tokens before generation.
+  /// Print prompt tokens before generation.
   bool verbosePrompt = false;
 
-  // Print prompt before generation.
+  /// Print prompt before generation.
   bool displayPrompt = true;
 
-  // Use infill mode.
+  /// Use infill mode.
   bool infill = false;
 
-  // Dump the KV cache contents for debugging purposes.
+  /// Dump the KV cache contents for debugging purposes.
   bool dumpKvCache = false;
 
-  // Disable KV offloading.
+  /// Disable KV offloading.
   bool noKvOffload = false;
 
-  // KV cache data type for the K.
+  /// KV cache data type for the K.
   String cacheTypeK = '';
   
-  // KV cache data type for the V.
+  /// KV cache data type for the V.
   String cacheTypeV = '';
 
-  // Path to multimodal projector.
+  /// Path to multimodal projector.
   String mmproj = '';
 
-  // Path to an image file.
+  /// Path to an image file.
   String image = '';
 
   Pointer<gpt_c_params> get() {
--- lib/src/maid_llm.dart
+++ lib/src/maid_llm.dart
@@ -4,11 +4,13 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:ffi/ffi.dart';
+import 'package:maid_llm/src/gpt_params.dart';
 
 import 'bindings.dart';
 
 class MaidLLM {
   static maid_llm? _lib;
+  static void Function(String)? _log;
 
   /// Getter for the Llama library.
   ///
@@ -27,5 +29,16 @@ class MaidLLM {
       }
     }
     return _lib!;
+  }
+
+  MaidLLM(GptParams params, {void Function(String)? log}) {
+    _log = log;
+    lib.maid_llm_init(params.get(), Pointer.fromFunction(_logOutput));
+  }
+
+  static void _logOutput(Pointer<Char> message) {
+    if (_log != null) {
+      _log!(message.cast<Utf8>().toDartString());
+    }
   }
 }
