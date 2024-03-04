d9287ce99e8d6d7fc3226398a009d0040805871c
Dane Madsen <dane_madsen@hotmail.com>
2024-03-04 11:06:23 +1000

    Use langchain ChatMessages

--- lib/maid_llm.dart
+++ lib/maid_llm.dart
@@ -3,3 +3,5 @@ library;
 export 'src/sampling_params.dart';
 export 'src/gpt_params.dart';
 export 'src/maid_llm.dart';
+
+export 'package:langchain/langchain.dart';
--- lib/src/maid_llm.dart
+++ lib/src/maid_llm.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
+import 'package:langchain/langchain.dart';
 import 'gpt_params.dart';
 
 import 'bindings.dart';
@@ -37,12 +38,12 @@ class MaidLLM {
     lib.maid_llm_init(params.get(), Pointer.fromFunction(_logOutput));
   }
 
-  Stream<String> prompt(String input) async* {
+  Stream<String> prompt(List<ChatMessage> messages) async* {
     final receivePort = ReceivePort();
     _sendPort = receivePort.sendPort;
 
-    final isolate  = await Isolate.spawn(_promptIsolate, (input, _sendPort!));
-    
+    final isolate = await Isolate.spawn(_promptIsolate, (messages, _sendPort!));
+
     await for (var data in receivePort) {
       if (data is (String, bool)) {
         final (message, done) = data;
@@ -58,10 +59,36 @@ class MaidLLM {
     }
   }
 
-  static void _promptIsolate((String, SendPort) args) {
-    final (input, sendPort) = args;
+  static void _promptIsolate((List<ChatMessage>, SendPort) args) {
+    final (messages, sendPort) = args;
     _sendPort = sendPort;
-    lib.maid_llm_prompt(input.toNativeUtf8().cast<Char>(), Pointer.fromFunction(_output));
+    lib.maid_llm_prompt(
+        _toNativeChatMessages(messages), Pointer.fromFunction(_output));
+  }
+
+  static Pointer<Pointer<chat_message>> _toNativeChatMessages(
+      List<ChatMessage> messages) {
+    final chatMessages = calloc<Pointer<chat_message>>(messages.length);
+
+    for (var i = 0; i < messages.length; i++) {
+      chatMessages[i] = calloc<chat_message>()
+        ..ref.role = _chatMessageToRole(messages[i])
+        ..ref.content = messages[i].contentAsString.toNativeUtf8().cast<Char>();
+    }
+
+    return chatMessages;
+  }
+
+  static int _chatMessageToRole(ChatMessage message) {
+    if (message is SystemChatMessage) {
+      return chat_role.ROLE_SYSTEM;
+    } else if (message is HumanChatMessage) {
+      return chat_role.ROLE_USER;
+    } else if (message is AIChatMessage) {
+      return chat_role.ROLE_ASSISTANT;
+    } else {
+      throw Exception('Unknown ChatMessage type');
+    }
   }
 
   static void _output(Pointer<Char> buffer, bool stop) {
@@ -81,7 +108,7 @@ class MaidLLM {
   void stop() {
     lib.maid_llm_stop();
   }
-  
+
   void clear() {
     lib.maid_llm_cleanup();
   }
