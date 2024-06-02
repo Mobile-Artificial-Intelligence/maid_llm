b7dcbb9fece03e8a6099360a3c7927490687dc53
danemadsen <dane_madsen@hotmail.com>
2024-06-02 16:55:05 +1000

    add ios

---
+++ ios/Classes/maid_llm.cpp
@@ -0,0 +1,5 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../{projectName}}.podspec for more information.
+#include "../../src/maid_llm.cpp"
+#include "../../src/maid_llm.h"
+#include "../../src/utils.hpp"
